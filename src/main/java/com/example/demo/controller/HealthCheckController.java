337a92f47cb1d2ca828428b194ae97f69991f919
Park JaeHong <blogerhong@naver.com>
2024-12-19 14:02:46 +0900

    feat : health check API 추가

---
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "서버 정상 작동 중 !";
+    }
+}
